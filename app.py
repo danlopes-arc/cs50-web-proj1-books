4b2ee95ac96d74a5bfbfe613118358dc3c4a4580
danlopes-arc <danlpsarcanjo@gmail.com>
2020-05-03 18:28:52 -0400

    Created layout.html and index.html Adjusted some stuff in app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
+import requests
 
-from flask import Flask, session
+from flask import Flask, session, redirect, render_template, request, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from tempfile import mkdtemp
 
 DATABASE_URL = "postgres://wrsgwrmkrvjnkg:315f2ff023c1d9b3d063fbd2c8c0ec20fe50daae75d8c89525821bfc7c088ffb@ec2-54-88-130-244.compute-1.amazonaws.com:5432/dde9dia2t0hl7o"
+GOODREADS_KEY = "9gRUykfyjGUMc0qT9sRrjw"
 
 app = Flask(__name__)
 
@@ -14,6 +17,7 @@ app = Flask(__name__)
 #     raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
+app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
@@ -26,4 +30,6 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    # res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": GOODREADS_KEY, "isbns": "9781632168146"})
+    session["user_id"] = 12
+    return render_template("index.html")
