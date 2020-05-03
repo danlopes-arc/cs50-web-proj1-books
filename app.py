e6317fa4a004ba2c14085ec0ceb5ef478fb2a741
danlopes-arc <danlpsarcanjo@gmail.com>
2020-05-03 13:49:10 -0400

    Update app.py Move database url to constant

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+DATABASE_URL = "postgres://wrsgwrmkrvjnkg:315f2ff023c1d9b3d063fbd2c8c0ec20fe50daae75d8c89525821bfc7c088ffb@ec2-54-88-130-244.compute-1.amazonaws.com:5432/dde9dia2t0hl7o"
+
 app = Flask(__name__)
 
 # Check for environment variable
@@ -18,7 +20,7 @@ Session(app)
 
 # Set up database
 # engine = create_engine(os.getenv("DATABASE_URL"))
-engine = create_engine("postgres://wrsgwrmkrvjnkg:315f2ff023c1d9b3d063fbd2c8c0ec20fe50daae75d8c89525821bfc7c088ffb@ec2-54-88-130-244.compute-1.amazonaws.com:5432/dde9dia2t0hl7o")
+engine = create_engine(DATABASE_URL)
 db = scoped_session(sessionmaker(bind=engine))
 
 
