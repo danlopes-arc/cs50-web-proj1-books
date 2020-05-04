ae3e33c55219536fb1f65221f3aea5a0fe8c64ea
danlopes-arc <danlpsarcanjo@gmail.com>
2020-05-03 23:20:33 -0400

    Add a bunch of files. Changed layout.html

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 from flask import Flask, session, redirect, render_template, request, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
+from sqlalchemy.sql import text
 from sqlalchemy.orm import scoped_session, sessionmaker
 from tempfile import mkdtemp
 
